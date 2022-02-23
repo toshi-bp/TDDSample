decc3cfbbabc504d46f2afa9f4537552aa451712
toshi-bp <yttoshi-bp@outlook.jp>
2022-02-23 17:09:44 +0900

    重複の削除

--- money/Dollar.java
+++ money/Dollar.java
@@ -1,11 +1,11 @@
 package money;
 
 public class Dollar {
-    int amount = 10;
+    int amount;
     Dollar (int amount) {
-
+        this.amount = amount;
     }
     void times (int multiplier) {
-
+        amount = amount * multiplier;
     }
 }
