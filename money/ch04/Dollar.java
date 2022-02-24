0f02a2a502eedd4a7c064521cd993d5524a19f10
toshi-bp <yttoshi-bp@outlook.jp>
2022-02-24 15:23:12 +0900

    ch04 準備

---
+++ money/ch04/Dollar.java
@@ -0,0 +1,15 @@
+package money.ch04;
+
+public class Dollar {
+    int amount;
+    Dollar (int amount) {
+        this.amount = amount;
+    }
+    Dollar times (int multiplier) {
+        return new Dollar(amount * multiplier);
+    }
+    public boolean equals(Object object) {
+        Dollar dollar = (Dollar) object;
+        return amount == dollar.amount;
+    }
+}
