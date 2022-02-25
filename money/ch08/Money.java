4dacc8157b61d356bb7a8a9ee4ca86964f13762d
toshi-bp <yttoshi-bp@outlook.jp>
2022-02-26 01:06:38 +0900

    ch08: done

--- money/ch08/Money.java
+++ money/ch08/Money.java
@@ -1,9 +1,16 @@
 package money.ch08;
 
-public class Money {
+public abstract class Money {
     protected int amount;
+    abstract Money times(int multiplier);
     public boolean equals(Object object) {
         Money money = (Money) object;
         return amount == money.amount && getClass().equals(money.getClass());
+    }
+    static Money dollar (int amount) {
+        return new Dollar (amount);
+    }
+    static Money franc (int amount) {
+        return new Franc(amount);
     }
 }
