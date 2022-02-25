a0b1e3ffe23135f79f9648c2a9aec2633fd54904
toshi-bp <yttoshi-bp@outlook.jp>
2022-02-26 00:26:58 +0900

    ch07: done

--- money/ch07/Money.java
+++ money/ch07/Money.java
@@ -4,6 +4,6 @@ public class Money {
     protected int amount;
     public boolean equals(Object object) {
         Money money = (Money) object;
-        return amount == money.amount;
+        return amount == money.amount && getClass().equals(money.getClass());
     }
 }
