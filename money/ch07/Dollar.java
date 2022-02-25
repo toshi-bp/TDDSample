61c0a38e5938d8143fd8307c5b29363cf5276e55
toshi-bp <yttoshi-bp@outlook.jp>
2022-02-25 16:17:10 +0900

    ch07: 下準備

---
+++ money/ch07/Dollar.java
@@ -0,0 +1,10 @@
+package money.ch07;
+
+public class Dollar extends Money{
+    Dollar (int amount) {
+        this.amount = amount;
+    }
+    Dollar times (int multiplier) {
+        return new Dollar(amount * multiplier);
+    }
+}
