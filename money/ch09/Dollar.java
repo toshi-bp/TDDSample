437ee95516620123b4b3e98a85ef7041414ef14b
toshi-bp <yttoshi-bp@outlook.jp>
2022-02-26 13:33:42 +0900

    ch09: dollarとfrancのcurrencyの実装を同一のものとした

--- money/ch09/Dollar.java
+++ money/ch09/Dollar.java
@@ -1,11 +1,13 @@
 package money.ch09;
 
 public class Dollar extends Money{
+    private String currency;
     Dollar (int amount) {
         this.amount = amount;
+        currency = "USD";
     }
     String currency() {
-        return "USD";
+        return currency;
     }
     Money times (int multiplier) {
         return new Dollar(amount * multiplier);
--- money/ch09/Franc.java
+++ money/ch09/Franc.java
@@ -1,11 +1,13 @@
 package money.ch09;
 
 public class Franc extends Money{
+    private String currency;
     Franc (int amount) {
         this.amount = amount;
+        currency = "CHF";
     }
     String currency() {
-        return "CHF";
+        return currency;
     }
     Money times (int multiplier) {
         return new Franc(amount * multiplier);
